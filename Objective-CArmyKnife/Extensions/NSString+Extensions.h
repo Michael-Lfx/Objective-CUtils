8a5f315cb5ef156298d8af423cd522b363dcffef
Michael-LFX <cihv2@163.com>
2015-06-03 14:03:38 +0800

    NSString拓展

---
+++ Objective-CArmyKnife/Extensions/NSString+Extensions.h
@@ -0,0 +1,41 @@
+@import Foundation;
+@import UIKit;
+
+@interface NSString (Base64)
+
++ (NSString *)base64StringFromData:(NSData *)data length:(int)length;
+
+@end
+
+@interface NSString (Color)
+
+- (UIColor *)color;
+
+@end
+
+@interface NSString (Regex)
+
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement;
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement caseInsensitive:(BOOL)ignoreCase;
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement caseInsensitive:(BOOL)ignoreCase treatAsOneLine:(BOOL)assumeMultiLine;
+- (NSArray *)stringsByExtractingGroupsUsingRegexPattern:(NSString *)regex;
+- (NSArray *)stringsByExtractingGroupsUsingRegexPattern:(NSString *)regex caseInsensitive:(BOOL) ignoreCase treatAsOneLine:(BOOL)assumeMultiLine;
+- (BOOL)matchesPatternRegexPattern:(NSString *)regex;
+- (BOOL)matchesPatternRegexPattern:(NSString *)regex caseInsensitive:(BOOL)ignoreCase treatAsOneLine:(BOOL)assumeMultiLine;
+
+@end
+
+@interface NSString (AES)
+
++ (NSString *)encrypt:(NSString *)message password:(NSString *)password;
++ (NSString *)decrypt:(NSString *)base64EncodedString password:(NSString *)password;
+
+@end
+
+@interface NSString (Bundle)
+
++ (NSString *)stringFromMainBundleFilename:(NSString *)filename;
+
+@end
+
+
---
+++ Objective-CArmyKnife/Extensions/NSString+Extensions.m
@@ -0,0 +1,206 @@
+#import "NSString+Extensions.h"
+
+#pragma mark - Base64
+
+static char base64EncodingTable[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+@implementation NSString (Base64)
+
++ (NSString *)base64StringFromData:(NSData *)data length:(int)length {
+    unsigned long ixtext, lentext;
+    long ctremaining;
+    unsigned char input[3], output[4];
+    short i, charsonline = 0, ctcopy;
+    const unsigned char *raw;
+    NSMutableString *result;
+    
+    lentext = [data length];
+    if (lentext < 1) {
+        return @"";
+    }
+    result = [NSMutableString stringWithCapacity: lentext];
+    raw = [data bytes];
+    ixtext = 0;
+    
+    while (true) {
+        ctremaining = lentext - ixtext;
+        if (ctremaining <= 0) {
+            break;
+        }
+        for (i = 0; i < 3; i++) {
+            unsigned long ix = ixtext + i;
+            if (ix < lentext) {
+                input[i] = raw[ix];
+            }
+            else {
+                input[i] = 0;
+            }
+        }
+        output[0] = (input[0] & 0xFC) >> 2;
+        output[1] = ((input[0] & 0x03) << 4) | ((input[1] & 0xF0) >> 4);
+        output[2] = ((input[1] & 0x0F) << 2) | ((input[2] & 0xC0) >> 6);
+        output[3] = input[2] & 0x3F;
+        ctcopy = 4;
+        switch (ctremaining) {
+            case 1:
+                ctcopy = 2;
+                break;
+            case 2:
+                ctcopy = 3;
+                break;
+        }
+        
+        for (i = 0; i < ctcopy; i++) {
+            [result appendString: [NSString stringWithFormat: @"%c", base64EncodingTable[output[i]]]];
+        }
+        
+        for (i = ctcopy; i < 4; i++) {
+            [result appendString: @"="];
+        }
+        
+        ixtext += 3;
+        charsonline += 4;
+        
+        if ((length > 0) && (charsonline >= length)) {
+            charsonline = 0;
+        }
+    }
+    return result;
+}
+
+@end
+
+#pragma mark - UIColor
+
+//@implementation NSString (Color)
+//
+//- (UIColor *)color {
+//    return [UIColor colorWithHexString:self];
+//}
+//
+//@end
+
+#pragma mark - 正则
+// 整合自 https://github.com/carlbrown/RegexOnNSString
+
+@implementation NSString (Regex)
+
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement caseInsensitive:(BOOL)ignoreCase {
+    return [self stringByReplacingRegexPattern:regex withString:replacement caseInsensitive:ignoreCase treatAsOneLine:NO];
+}
+
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement caseInsensitive:(BOOL)ignoreCase treatAsOneLine:(BOOL)assumeMultiLine {
+    
+    NSUInteger options=0;
+    if (ignoreCase) {
+        options = options | NSRegularExpressionCaseInsensitive;
+    }
+    if (assumeMultiLine) {
+        options = options | NSRegularExpressionDotMatchesLineSeparators;
+    }
+    
+    NSError *error=nil;
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:&error];
+    if (error) {
+        NSLog(@"Error creating Regex: %@",[error description]);
+        return nil;
+    }
+    
+    NSString *retVal= [pattern stringByReplacingMatchesInString:self options:0 range:NSMakeRange(0, [self length]) withTemplate:replacement];
+    return retVal;
+}
+
+- (NSString *)stringByReplacingRegexPattern:(NSString *)regex withString:(NSString *)replacement {
+    return [self stringByReplacingRegexPattern:regex withString:replacement caseInsensitive:NO treatAsOneLine:NO];
+}
+
+- (NSArray *)stringsByExtractingGroupsUsingRegexPattern:(NSString *)regex caseInsensitive:(BOOL)ignoreCase treatAsOneLine:(BOOL)assumeMultiLine {
+    NSUInteger options=0;
+    if (ignoreCase) {
+        options = options | NSRegularExpressionCaseInsensitive;
+    }
+    if (assumeMultiLine) {
+        options = options | NSRegularExpressionDotMatchesLineSeparators;
+    }
+    
+    NSError *error=nil;
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:&error];
+    if (error) {
+        NSLog(@"Error creating Regex: %@",[error description]);
+        return nil;
+    }
+    
+    __block NSMutableArray *retVal = [NSMutableArray array];
+    [pattern enumerateMatchesInString:self options:0 range:NSMakeRange(0, [self length]) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+        //Note, we only want to return the things in parens, so we're skipping index 0 intentionally
+        for (int i=1; i<[result numberOfRanges]; i++) {
+            NSString *matchedString=[self substringWithRange:[result rangeAtIndex:i]];
+            [retVal addObject:matchedString];
+        }
+    }];
+    return retVal;
+}
+
+- (NSArray *)stringsByExtractingGroupsUsingRegexPattern:(NSString *)regex {
+    return [self stringsByExtractingGroupsUsingRegexPattern:regex caseInsensitive:NO treatAsOneLine:NO];
+}
+
+- (BOOL)matchesPatternRegexPattern:(NSString *)regex caseInsensitive:(BOOL)ignoreCase treatAsOneLine:(BOOL)assumeMultiLine {
+    NSUInteger options=0;
+    if (ignoreCase) {
+        options = options | NSRegularExpressionCaseInsensitive;
+    }
+    if (assumeMultiLine) {
+        options = options | NSRegularExpressionDotMatchesLineSeparators;
+    }
+    
+    NSError *error=nil;
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:&error];
+    if (error) {
+        NSLog(@"Error creating Regex: %@",[error description]);
+        return NO;  //Can't possibly match an invalid Regex
+    }
+    
+    return ([pattern numberOfMatchesInString:self options:0 range:NSMakeRange(0, [self length])] > 0);
+}
+
+- (BOOL)matchesPatternRegexPattern:(NSString *)regex {
+    return [self matchesPatternRegexPattern:regex caseInsensitive:NO treatAsOneLine:NO];
+}
+
+@end
+
+#pragma mark - AES加解密
+
+//@implementation NSString (AES)
+//
+//+ (NSString *)encrypt:(NSString *)message password:(NSString *)password {
+//    NSData *encryptedData = [[message dataUsingEncoding:NSUTF8StringEncoding] AES256EncryptedDataUsingKey:[[password dataUsingEncoding:NSUTF8StringEncoding] SHA256Hash] error:nil];
+//    NSString *base64EncodedString = [NSString base64StringFromData:encryptedData length:[encryptedData length]];
+//    return base64EncodedString;
+//}
+//
+//+ (NSString *)decrypt:(NSString *)base64EncodedString password:(NSString *)password {
+//    NSData *encryptedData = [NSData base64DataFromString:base64EncodedString];
+//    NSData *decryptedData = [encryptedData decryptedAES256DataUsingKey:[[password dataUsingEncoding:NSUTF8StringEncoding] SHA256Hash] error:nil];
+//    return [[NSString alloc] initWithData:decryptedData encoding:NSUTF8StringEncoding];
+//}
+//
+//@end
+
+#pragma mark - NSBundle
+
+@implementation NSString (Bundle)
+
++ (NSString *)stringFromMainBundleFilename:(NSString *)filename {
+    NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:nil];
+    NSString *text = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    return text;
+}
+
+@end
