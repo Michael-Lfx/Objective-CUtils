73f73443ecb4487ee1c690be0ca6559db86ff060
Michael-LFX <cihv2@163.com>
2015-06-04 20:32:46 +0800

    限制自动属性为模型类，不再支持UIKit等框架类。

--- Objective-CArmyKnife/Extensions/NSObject+Extensions.h
+++ Objective-CArmyKnife/Extensions/NSObject+Extensions.h
@@ -1,7 +1,17 @@
 @import Foundation;
 
+extern void deleteLastTwoCharacters(NSMutableString *string);
+
 @interface NSObject (Description)
 
-@property (copy, nonatomic, readonly) NSString *autoDescription;
+/**
+*  输出对象及其继承的属性值，只支持自定义模型类
+*/
+- (NSString *)autoDescription;
+
+/**
+ *  输出可便于阅读的内容
+ */
+- (NSString *)readableDescription;
 
 @end
--- Objective-CArmyKnife/Extensions/NSObject+Extensions.m
+++ Objective-CArmyKnife/Extensions/NSObject+Extensions.m
@@ -5,41 +5,60 @@
 
 #import <objc/runtime.h>
 
-@implementation NSObject (Description)
-
-- (NSString *)enumeratePropertiesAndValues:(Class)cls {
-    NSMutableString * result = [NSMutableString string];
-    
-    Class superClass  = class_getSuperclass(cls);
-    if (superClass != nil && ![superClass isEqual:[NSObject class]]) {
-        [result appendString:[self enumeratePropertiesAndValues:superClass]];
-    }
+static NSString* makePropertiesAndValuesString(Class cls, id obj) {
+    NSMutableString *result = [NSMutableString string];
     
     uint property_count;
-    objc_property_t * property_list = class_copyPropertyList(cls, &property_count);
-    
-    for (int i = property_count - 1; i >= 0; --i) { // Reverse order, to get Properties in order they were defined
+    objc_property_t *property_list = class_copyPropertyList(cls, &property_count);
+    for (int i = 0; i < property_count; i++) {
         objc_property_t property = property_list[i];
-        
-        const char * property_name = property_getName(property);
-        NSString * propertyName = [NSString stringWithCString:property_name encoding:NSASCIIStringEncoding];
+        const char *property_name = property_getName(property);
+        NSString *propertyName = [NSString stringWithCString:property_name encoding:NSASCIIStringEncoding];
         if (propertyName) {
-            id value = [self valueForKey:propertyName];
-            [result appendFormat:@"%@ = %@; ", propertyName, value];
+            id value = [obj valueForKey:propertyName];
+            [result appendFormat:@"%@ = %@; ", propertyName, [value readableDescription]];
         }
     }
-    free(property_list);
     
-    if (result.length > 0) {
-        [result deleteCharactersInRange:NSMakeRange(result.length - 2, 2)];
-    }
+    if (property_list)  free(property_list);
     
     return result;
 }
 
+inline void deleteLastTwoCharacters(NSMutableString *string) {
+    NSUInteger length = [string length];
+    if (length > 2) {
+        [string deleteCharactersInRange:NSMakeRange(length - 2, 2)];
+    }
+}
+
+@implementation NSObject (Description)
+
+/**
+ *  采用非递归实现。尝试了支持所有类，在处理UIKit时遇到取出的属性要么不存在，要么不支持KVC，故放弃。
+ *
+ *  @return 对象本身定义的属性及其继承的属性与值组成的字符串，如：p1 = v1; p2 = v2
+ */
+- (NSString *)enumeratePropertiesAndValues {
+    @autoreleasepool {
+        NSMutableString *result = [NSMutableString string];
+        
+        Class cls = self.class;
+        while (cls != nil && ![cls isEqual:NSObject.class]) {
+            [result appendString:makePropertiesAndValuesString(cls, self)];
+            cls = class_getSuperclass(cls);
+        }
+        deleteLastTwoCharacters(result);
+        return [result copy];
+    }
+}
+
 - (NSString *)autoDescription {
-    Class currentClass = [self class];
-    return [NSString stringWithFormat:@"<%@: %p> { %@ }", NSStringFromClass(currentClass), self, [self enumeratePropertiesAndValues:currentClass]];
+    return [NSString stringWithFormat:@"<%@: %p; %@>", NSStringFromClass(self.class), self, [self enumeratePropertiesAndValues]];
+}
+
+- (NSString *)readableDescription {
+    return [self description];
 }
 
 @end
