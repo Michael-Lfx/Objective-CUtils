36b27888894e44decb839abf07ab2f8f165dc632
Michael-LFX <cihv2@163.com>
2015-06-08 16:03:44 +0800

    优化混写代码

--- Objective-CArmyKnifeExample/Objective-CArmyKnife/RuntimeExtensions/MethodSwizzConvenienceInterface/MethodSwizzConvenienceInterface.m
+++ Objective-CArmyKnifeExample/Objective-CArmyKnife/RuntimeExtensions/MethodSwizzConvenienceInterface/MethodSwizzConvenienceInterface.m
@@ -1,41 +1,46 @@
 #import "MethodSwizzConvenienceInterface.h"
 #import <objc/runtime.h>
 
-void swizz(Class cls, SEL srcSelector, SEL dstSelector, Method srcMethod, Method dstMethod);
-void swizzClassMethod(Class cls, SEL srcSelector, SEL dstSelector);
-void swizzInstanceMethod(Class cls, SEL srcSelector, SEL dstSelector);
-BOOL validateMethod(Method srcMethod, Method dstMethod);
+void swizz(Class cls, SEL, SEL, Method, Method);
+BOOL swizzClassMethod(Class, SEL srcSelector, SEL);
+BOOL swizzInstanceMethod(Class, SEL, SEL);
+BOOL trySwizz(Class cls, SEL, SEL, Method, Method);
 
 inline void methodSwizz(Class cls, SEL srcSelector, SEL dstSelector) {
-    swizzInstanceMethod(cls, srcSelector, dstSelector);
-    swizzClassMethod(cls, srcSelector, dstSelector);
+    if (swizzInstanceMethod(cls, srcSelector, dstSelector) ||
+        swizzClassMethod(cls, srcSelector, dstSelector))
+    {
+        return;
+    }
+    printf("\n=====混写失败：srcSelector或dstSelector对应的Method不存在=====\n\n");
 }
 
 #pragma mark - 私有实现
 
-inline void swizzInstanceMethod(Class cls, SEL srcSelector, SEL dstSelector) {
+inline BOOL swizzInstanceMethod(Class cls, SEL srcSelector, SEL dstSelector) {
     Method srcMethod = class_getInstanceMethod(cls, srcSelector);
     Method dstMethod = class_getInstanceMethod(cls, dstSelector);
-    swizz(cls, srcSelector, dstSelector, srcMethod, dstMethod);
+    return trySwizz(cls, srcSelector, dstSelector, srcMethod, dstMethod);
 }
 
-inline void swizzClassMethod(Class cls, SEL srcSelector, SEL dstSelector) {
+inline BOOL swizzClassMethod(Class cls, SEL srcSelector, SEL dstSelector) {
     Method srcMethod = class_getClassMethod(cls, srcSelector);
     Method dstMethod = class_getClassMethod(cls, dstSelector);
-    if (validateMethod(srcMethod, dstMethod)) {
-        printf("\n混写失败：srcSelector或dstSelector对应的Method不存在\n");
-        return;
-    }
-    swizz(cls, srcSelector, dstSelector, srcMethod, dstMethod);
+    return trySwizz(cls, srcSelector, dstSelector, srcMethod, dstMethod);
 }
 
-inline BOOL validateMethod(Method srcMethod, Method dstMethod) {
-    return (srcMethod == NULL || dstMethod == NULL) ? FALSE : TRUE;
+inline BOOL trySwizz(Class cls, SEL srcSelector, SEL dstSelector, Method srcMethod, Method dstMethod) {
+    if ((srcMethod == NULL || dstMethod == NULL)) {
+        return FALSE;
+    }
+    swizz(cls, srcSelector, dstSelector, srcMethod, dstMethod);
+    return TRUE;
 }
 
 inline void swizz(Class cls, SEL srcSelector, SEL dstSelector, Method srcMethod, Method dstMethod) {
     @synchronized(cls) {
-        if (class_addMethod(cls, srcSelector, method_getImplementation(dstMethod), method_getTypeEncoding(dstMethod))) {
+        BOOL didAddMethod = class_addMethod(cls, srcSelector, method_getImplementation(dstMethod), method_getTypeEncoding(dstMethod));
+        if (didAddMethod) {
             class_replaceMethod(cls, dstSelector, method_getImplementation(srcMethod), method_getTypeEncoding(srcMethod));
         } else {
             method_exchangeImplementations(srcMethod, dstMethod);
