20c3a29ca06fe2d27edb483e00e230e987778bc1
Michael-LFX <cihv2@163.com>
2015-06-11 16:50:48 +0800

    补充注释

--- Objective-CArmyKnifeExample/Objective-CArmyKnife/UIKitExtensions/MoveCell/UITableView+MoveCell.h
+++ Objective-CArmyKnifeExample/Objective-CArmyKnife/UIKitExtensions/MoveCell/UITableView+MoveCell.h
@@ -22,6 +22,12 @@ typedef void(^MoveCellDataSourceUpdateBlock)(NSIndexPath *dstIndexPath, NSIndexP
  
  }];
  
+ 开始用二维指针传递数据源的方式实现，想把整个流程做得更自动化。发现要照顾的细节较多，故改成block由调用方决定数据源的同步操作。
+ 
+ 参考自：http://www.raywenderlich.com/63089/cookbook-moving-table-view-cells-with-a-long-press-gesture
+ 
+ TODO:UICollectionView实现类似功能
+ 
  *  @param block 更新数据源
  *
  *  @return 表视图
