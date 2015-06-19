ef5044d26dc41e3e807cff3af9145a4e511557db
Michael-LFX <cihv2@163.com>
2015-06-19 10:56:14 +0800

    1. 代码优化 2. 注释

--- Objective-CArmyKnife/CoreGraphicsExtensions/CGRect/CGRect+Convenience.h
+++ Objective-CArmyKnife/CoreGraphicsExtensions/CGRect/CGRect+Convenience.h
@@ -5,10 +5,35 @@
  *
  *  @param size 矩形大小
  *
- *  @return 新创建的矩形
+ *  @return 新建的矩形
  */
 OBJC_EXPORT CGRect CGRectFromCGSize(CGSize size);
 
+/**
+ *  根据中心点及大小创建矩形
+ *
+ *  @param center 中心点坐标
+ *  @param size   大小
+ *
+ *  @return 新建的矩形
+ */
 OBJC_EXPORT CGRect CGRectMakeWithCenterAndSize(CGPoint center, CGSize size);
+
+/**
+ *  通过起点坐标及大小创建矩形
+ *
+ *  @param origin 起点坐标
+ *  @param size   大小
+ *
+ *  @return 新建的矩形
+ */
 OBJC_EXPORT CGRect CGRectMakeWithOriginAndSize(CGPoint origin, CGSize size);
+
+/**
+ *  返回矩形中心点坐标
+ *
+ *  @param rect 矩形
+ *
+ *  @return 中心点坐标
+ */
 OBJC_EXPORT CGPoint CGRectCenter(CGRect rect);
--- Objective-CArmyKnife/CoreGraphicsExtensions/CGRect/CGRect+Convenience.m
+++ Objective-CArmyKnife/CoreGraphicsExtensions/CGRect/CGRect+Convenience.m
@@ -1,17 +1,17 @@
 #import "CGRect+Convenience.h"
 
 inline CGRect CGRectFromCGSize(CGSize size) {
-    return (CGRect){.origin = CGPointZero, .size = size};
+    return (CGRect){ .origin = CGPointZero, .size = size };
 }
 
 inline CGRect CGRectMakeWithCenterAndSize(CGPoint center, CGSize size) {
-    return CGRectMake(center.x - size.width * 0.5, center.y - size.height * 0.5, size.width, size.height);
+    return (CGRect){ center.x - size.width * 0.5, center.y - size.height * 0.5, size };
 }
 
 inline CGRect CGRectMakeWithOriginAndSize(CGPoint origin, CGSize size) {
-    return CGRectMake(origin.x, origin.y, size.width, size.height);
+    return (CGRect){ .origin = origin, .size = size };
 }
 
 inline CGPoint CGRectCenter(CGRect rect) {
-    return CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
+    return (CGPoint){ CGRectGetMidX(rect), CGRectGetMidY(rect) };
 }
